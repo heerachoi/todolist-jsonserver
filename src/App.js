db26d9d04179af42091b753367c739fd54507a4d
heerachoi <heera7722@gmail.com>
2022-12-23 04:24:05 +0900

    [feat] Connected with JSON Server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-import './App.css';
+const App = () => {
+  // 새롭게 생성되는 todo를 관리합니다.
+  const [todos, setTodos] = useState(null);
 
-function App() {
-  return <div className='App'>Todo List</div>;
-}
+  // Todos 데이터를 서버에서 가져옵니다.
+  const fetchTodos = async () => {
+    const { data } = await axios.get('http://localhost:3001/todos');
+    setTodos(data);
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  console.log(todos); // App.js:16
+  return <div>App</div>;
+};
 
 export default App;
